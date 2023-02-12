49809c50b80bf4c98c1f7f7a4eda4d4ae6617beb
Radosław Maciaszczyk <rmaciaszczyk@zut.edu.pl>
2023-02-12 19:50:44 +0100

    Final solution

--- app/src/main/java/edu/zut/erasmus_plus/location/MapsActivity.kt
+++ app/src/main/java/edu/zut/erasmus_plus/location/MapsActivity.kt
@@ -1,7 +1,16 @@
 package edu.zut.erasmus_plus.location
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -16,18 +25,114 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private var isRequestLoacation: Boolean = false
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            500)
+            .build()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                if (locationResult != null) {
+                    super.onLocationResult(locationResult)
+                    locationResult.lastLocation?.let {
+                        val myPosition = it?.let {
+                            LatLng(it.latitude,it.longitude)
+                        }
+                        myPosition?.let {
+                            mMap.addMarker(MarkerOptions().position(myPosition).title("My position"))
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(myPosition))
+
+                        }
+                    }
+
+                }
+            }
+        }
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
+    fun getLastPos(view: View)
+    {
+        checkPermission()
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                val myPosition = location?.let {
+                    LatLng(it.latitude,it.longitude)
+                }
+                myPosition?.let {
+                    mMap.addMarker(MarkerOptions().position(myPosition).title("My position"))
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(myPosition))
+                }
+            }
 
+    }
+
+    private fun checkPermission()
+    {
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED)
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {
+                isGranted: Boolean ->
+            if (isGranted) {
+                Log.i("Permission: ", "Granted")
+            } else {
+                Log.i("Permission: ", "Denied")
+            }
+        }
+
+    fun startStopRequestLocation(view: View)
+    {
+        checkPermission()
+        if (!isRequestLoacation)
+        {
+            binding.btContinousPosition.text=getString(R.string.stop_loop)
+            val addTask= fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+            addTask.addOnCompleteListener {task->
+                if (task.isSuccessful) {
+                    Log.d("startStopRequestLocation", "Start loop Location Callback.")
+                } else {
+                    Log.d("startStopRequestLocation", "Failed start  Location Callback.")
+                }
+            }
+        }
+        else
+        {
+            binding.btContinousPosition.text=getString(R.string.start_loop)
+            val removeTask = fusedLocationClient.removeLocationUpdates(locationCallback)
+            removeTask.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("startStopRequestLocation", "Location Callback removed.")
+                } else {
+                    Log.d("startStopRequestLocation", "Failed to remove Location Callback.")
+                }
+            }
+        }
+        isRequestLoacation=!isRequestLoacation
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
